23ed9c0edfaa4574e6b0a982f7488574e0e83398
bala-amypo <bala@amypo.in>
2025-12-19 10:17:34 +0000

    User commit

--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -1,9 +1,6 @@
 package com.example.demo.exception;
 
-// Custom exception used when a resource is not found
 public class ResourceNotFoundException extends RuntimeException {
-
-    // Constructor that accepts an error message
     public ResourceNotFoundException(String message) {
         super(message);
     }
