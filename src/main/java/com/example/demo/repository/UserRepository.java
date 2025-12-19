edd9dd11ffa744d78cde7d0f2638fd039ac21070
bala-amypo <bala@amypo.in>
2025-12-19 10:29:35 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.repository;
 
-import java.util.*;
+import com.example.demo.entity.User;
+import java.util.Optional;
 
 public interface UserRepository {
+
     User save(User user);
+
     Optional<User> findById(Long id);
+
     Optional<User> findByEmail(String email);
 }
