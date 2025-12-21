65ebe6991a34564c55be02fd18ff9961a9c01347
bala-amypo <bala@amypo.in>
2025-12-21 05:27:04 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SecurityConfig {
-    
+    // ❌ no filters, no beans, no auth rules
 }
