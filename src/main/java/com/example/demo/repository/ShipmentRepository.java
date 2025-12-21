fed89cb0e75549c344abb15975315fc363c664be
bala-amypo <bala@amypo.in>
2025-12-21 10:46:16 +0000

    User commit

--- src/main/java/com/example/demo/repository/ShipmentRepository.java
+++ src/main/java/com/example/demo/repository/ShipmentRepository.java
@@ -1,3 +1,4 @@
+/*
 package com.example.demo.repository;
 
 import com.example.demo.entity.Shipment;
@@ -8,4 +9,16 @@ import java.util.List;
 public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
 
     List<Shipment> findByVehicleId(Long vehicleId);
+}
+*/
+
+
+package com.example.demo.repository;
+
+import com.example.demo.entity.Shipment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
 }
