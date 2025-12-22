8b5366a1a15c7a92990f4e595ae85a5e11e1c69a
bala-amypo <bala@amypo.in>
2025-12-22 03:50:26 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -17,7 +17,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User register(User user) {
-        // NO password encoding for now
         if (user.getRole() == null) {
             user.setRole("USER");
         }
